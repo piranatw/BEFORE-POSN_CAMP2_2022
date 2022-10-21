8240e861732a62a0f440e207ccc2860cdd9fe8c9
Piranat W <116284366+piranatw@users.noreply.github.com>
2022-10-21 13:59:05 +0700

    Add files via upload

---
+++ GT_paintgold.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define MxN 10001
+#define pii(a,b) pair<int a,int b>
+
+using namespace std;
+const int di[4]={1,-1,0,0},dj[4]={0,0,1,-1};
+
+int main(){
+    cin.tie(0)->ios::sync_with_stdio(0);
+    int q,n,m;
+    cin >> q;
+    cin >> n >> m;
+    while(q--){
+        int opr,x;
+
+    }
+    return 0;
+}
