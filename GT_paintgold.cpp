32cdc1104bf20b5d00c7e5178d66420aeb98edba
Piranat W <116284366+piranatw@users.noreply.github.com>
2022-10-21 13:59:29 +0700

    Add files via upload

--- GT_paintgold.cpp
+++ GT_paintgold.cpp
@@ -1,18 +1,32 @@
 #include <bits/stdc++.h>
-#define MxN 10001
-#define pii(a,b) pair<int a,int b>
-
 using namespace std;
-const int di[4]={1,-1,0,0},dj[4]={0,0,1,-1};
-
+int a[10010][1001];
+int R[1010],C[1010];
 int main(){
-    cin.tie(0)->ios::sync_with_stdio(0);
-    int q,n,m;
-    cin >> q;
-    cin >> n >> m;
-    while(q--){
-        int opr,x;
-
+    int n,m,k;
+    cin >> n >> m >> k;
+    char opr;
+    while(k--){
+        cin >> opr;
+        if(opr=='R'){
+            int x;cin >> x;
+            R[x]++;
+            R[x]%=2;
+        }
+        if(opr=='C'){
+            int x;cin >> x;
+            C[x]++;
+            C[x]%=2;
+        }
     }
+    int cnt=0;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            a[i][j]=R[i]+C[j];
+            a[i][j]%=2;
+            if(a[i][j]) cnt++;
+        }
+    }
+    cout << cnt;
     return 0;
 }
